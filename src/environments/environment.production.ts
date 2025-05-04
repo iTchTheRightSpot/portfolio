856815bbaedaa87fa4ad66e5b64f1ddbdcb25d7a
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-05-04 04:26:56 -0400

    include https in environment production mode

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  domain: 'portfolio-api.emmanueluluabuike.workers.dev',
+  domain: 'https://portfolio-api.emmanueluluabuike.workers.dev',
   github: 'https://github.com/iTchTheRightSpot',
   linkedIn: 'https://www.linkedin.com/in/somto-uluabuike-2924ab217/',
   email: 'emmanueluluabuike@gmail.com'
