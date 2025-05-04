b2ad50ce61c0e48cc3a56a17686dac249feefa05
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-05-04 09:10:23 -0400

    use env files

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { catchError, map, of } from 'rxjs';
+import { environment } from '~env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AppService {
 
   readonly emit = (name: string) =>
     this.http
-      .post<HttpResponse<number>>(`https://portfolio-api.emmanueluluabuike.workers.dev?name=${name}`, {})
+      .post<HttpResponse<number>>(`${environment.domain}?name=${name}`, {})
       .pipe(
         map(res => res.status),
         catchError(err => of(err.status || 400))
